4b9645e3c28e32e8c4771aab68175a83ef84bda6
Guilherme Ribeiro <guilherme.rego@souunit.com.br>
2025-11-07 16:51:36 -0300

    Começar ajustes de css e js na tela2

--- js/tela2.js
+++ js/tela2.js
@@ -10,3 +10,35 @@ document.querySelector('.botao-enviar').addEventListener('click', function () {
                 window.location.href = 'tela5.html';
             }
         }); 
+        
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+const header = document.querySelector("header")
+
+
+if(isMobile.any()){
+    header.style.justifyContent = "center"
+    logo.style.marginLeft = "0px"
+    panel.style.margin = "5rem auto 5rem auto"
+
+    console.log("he's on phone!!!")
+}
