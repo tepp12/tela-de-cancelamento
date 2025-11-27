b565dbe7e9c149606e9040ed04d957e8865e251b
ElfeNss <joaoajux@gmail.com>
2025-11-27 01:53:57 -0300

    criação da barra de carregamento
    mudança dos botões usando âncora para eventListener, botão não-clicável antes da barra carregar e eventListener que espera a página ser carregada para iniciar a animação do botão.

--- js/tela3.js
+++ js/tela3.js
@@ -20,12 +20,36 @@ const isMobile = {
 };
 
 const header = document.querySelector("header")
+const logo = document.querySelector(".logo")
+
+document.addEventListener('DOMContentLoaded', () => {
+    var btncancel = document.getElementById('btn-cancelar');
+    var btnaceitar = document.getElementById('btn-aceitar');
+    var loadingBarFill = document.getElementById('loadingBarFill');
+    const delayTime = 5000; // 5 segundos
+    btncancel.disabled = true;
+
+
+  // Começa a animação de barra enchendo
+    loadingBarFill.style.width = '100%';
+
+  // Coloca um tempo para ativar o botão, após a animação
+    setTimeout(function() {
+        btncancel.disabled = false; // Ativa o botão
+        btncancel.querySelector('.loading-bar-container').style.display = 'none'; // Esconde a barra de carregamento 
+        }, delayTime);                                                      // caso a barra de carregamento tenha cor diferente do botão
+        btncancel.addEventListener('click', function() {
+        window.location.href = 'tela5.html';
+    });
+    btnaceitar.addEventListener('click', function() {
+        window.location.href = 'tela4.html';})
+});
 
 
 if(isMobile.any()){
     header.style.justifyContent = "center"
     logo.style.marginLeft = "0px"
-    panel.style.margin = "5rem auto 5rem auto"
+    
 
     console.log("he's on phone!!!")
 }
